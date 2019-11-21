e553062578ade3ec26006991cf8e79c4e4e60b75
topografsusanin <36396971+topografsusanin@users.noreply.github.com>
2019-11-21 08:22:20 +0300

    Initial commit

---
+++ Assets/_Scripts/Game.cs
@@ -0,0 +1,129 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+
+    #region Singleton
+    private Game() { }
+    private static Game _instance;
+    public static Game Instance
+    {
+        get
+        {
+            if(_instance == null) { _instance = new Game(); }
+            return _instance;
+        }
+        set { _instance = value; }
+    }
+    #endregion
+
+    #region Properties
+    public Vector3 POS
+    {
+        get { return _currentFigure.transform.position; }
+        set { _currentFigure.transform.position = value; }
+    }
+    
+    public GameObject CurrentFigure
+    {
+        get
+        {
+            return _currentFigure;
+        }
+    }
+    #endregion
+
+    #region Fields
+    [Header("Set In Inspector")]
+    [SerializeField] List<GameObject> _figures;
+
+    [Header("Set Dynamically")]
+    [SerializeField] private GameObject _currentFigure;
+    [SerializeField] private GameObject _nextFigure;
+
+    private float _beginTimeY;
+    private float _stepTimeY = 0.2f;
+    private float _beginTimeX;
+    private float _stepTimeX = 0.1f;
+    #endregion
+
+    void Awake()
+    {
+        Instance = this;
+
+        int _index = ChooseFigure();
+        _currentFigure = Instantiate<GameObject>(_figures[_index]);
+        float _xRandom = Random.Range(2, 4);
+        _currentFigure.transform.position = new Vector3(_xRandom, 8.2f, 0f);
+        _nextFigure = ShowNextFigure();
+
+        _beginTimeX = Time.time;
+        _beginTimeY = Time.time;
+    }
+    void Update()
+    {
+            float _currentTime = Time.time;
+            {
+                if (_currentTime - _beginTimeX >= _stepTimeX)
+                {
+                    if (Input.GetKey(KeyCode.A))
+                    {
+                        Vector3 _pos = POS;
+                        _pos.x -= 0.2f;
+                        POS = _pos;
+                    }
+
+                    if (Input.GetKey(KeyCode.D))
+                    {
+                        Vector3 _pos = POS;
+                        _pos.x += 0.2f;
+                        POS = _pos;
+                    }
+
+                    _beginTimeX = _currentTime;
+                }
+            }
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                _currentFigure.transform.Rotate(Vector3.forward, 90f);
+            }
+
+            if (_currentTime - _beginTimeY >= _stepTimeY)
+            {
+                Vector3 _pos = POS;
+                _pos.y -= 0.2f;
+                POS = _pos;
+                _beginTimeY = _currentTime;
+            }
+    }
+
+
+    private void SetCurrentFigure(GameObject _figure)
+    {
+        float _xRandom = Random.Range(2, 4);
+        _figure.transform.position = new Vector3(_xRandom, 8.2f, 0f);
+        _currentFigure = _figure;
+        _nextFigure = ShowNextFigure();
+    }
+
+    private GameObject ShowNextFigure()
+    {
+        int _index = ChooseFigure();
+        GameObject _go = Instantiate<GameObject>(_figures[_index]);
+        _go.transform.position = new Vector3(25f, 50f, 0f);
+        return _go;
+    }
+
+    private int ChooseFigure()
+    {
+       return Random.Range(0, _figures.Count);
+    }
+
+    public void ReplaceFigure()
+    {
+        SetCurrentFigure(_nextFigure);
+    }
+}
---
+++ Assets/_Scripts/Walls.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameFieldCreator : MonoBehaviour
+{
+    [SerializeField] private GameObject _brickPrefab;
+    [SerializeField] private GameObject _gridPrefab;
+    [SerializeField] private GameObject _wallsPrefab;
+    [SerializeField] GameObject _bottom;
+    [SerializeField] GameObject _leftWall;
+    [SerializeField] GameObject _rightWall;
+    [SerializeField] GameObject _gridAnchor;
+
+
+    void Start()
+    {
+        //bottom wall
+        for (float i = 0.2f; i < 5.4f; i+=0.2f)
+        {
+            GameObject _cubeWallBottom = Instantiate<GameObject>(_brickPrefab);
+            _cubeWallBottom.transform.position = new Vector3(i, 0f, 0f);
+            _cubeWallBottom.transform.SetParent(_bottom.transform);
+            _cubeWallBottom.transform.localScale = new Vector3(0.15f, 0.2f, 0.2f);
+        }
+
+        //walls
+        for (float i = 0.2f;  i<=8.2f; i+=0.2f) 
+        {
+            GameObject _cubeWallLeft = Instantiate<GameObject>(_wallsPrefab);
+            _cubeWallLeft.transform.position = new Vector3(0f, i, 0f);
+            _cubeWallLeft.transform.parent = _leftWall.transform; // same
+            _cubeWallLeft.transform.localScale = new Vector3(0.2f, 0.15f, 0.2f);
+
+            GameObject _cubeWallRight = Instantiate<GameObject>(_wallsPrefab);
+            _cubeWallRight.transform.position = new Vector3(5.6f, i, 0f);
+            _cubeWallRight.transform.SetParent(_rightWall.transform); // same
+            _cubeWallRight.transform.localScale = new Vector3(0.2f, 0.15f, 0.2f);
+        }
+
+        //grid
+        for (float i = 0.2f; i < 5.4f; i+=0.2f)
+        {
+            for (float j = 0.2f; j <= 7.2f; j+=0.2f)
+            {
+                GameObject _grid = Instantiate<GameObject>(_gridPrefab);
+                _grid.GetComponent<Renderer>().material.color = Color.green;
+                _grid.transform.position = new Vector3(i, j, 2f);
+                _grid.transform.localScale = new Vector3(0.03f, 0.03f, 0.03f);
+                _grid.transform.SetParent(_gridAnchor.transform);
+            }
+        }
+
+    }    
+}
