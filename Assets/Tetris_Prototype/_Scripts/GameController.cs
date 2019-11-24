883da0595d41546ba7a0bf2d622c54b93fb71c4f
topografsusanin <36396971+topografsusanin@users.noreply.github.com>
2019-11-24 05:12:37 +0300

    beta

---
+++ Assets/Tetris_Prototype/_Scripts/GameController.cs
@@ -0,0 +1,250 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    [Header("Set in Inspector")]
+    [SerializeField] private List<GameObject> _figurePrefabs;
+    [SerializeField] private Text _highScoreCount;
+
+    [Header("Set Dynamically")]
+    [SerializeField] private GameObject _currentFigure;
+    [SerializeField] private GameObject _nextFigure;
+
+    private Transform[] _coordsCurrentFigure;
+    private Transform[,] _coordXYFigures;
+
+    private float _beginTime;
+    private bool _isMovingDown = true;
+    private int _score = 0;
+    private int _countOfDeleatedLines;
+
+
+    void Start()
+    {
+        _coordXYFigures = new Transform[10, 25];
+
+        int _randomCurFigure = UnityEngine.Random.Range(0, 7);
+        _currentFigure = Instantiate<GameObject>(_figurePrefabs[_randomCurFigure]);
+
+        if (_currentFigure.tag == "NeedCorrectPosition")
+        {
+            _currentFigure.transform.position = new Vector3(4.5f, 20.5f, 0f);
+        }
+        else
+        {
+            _currentFigure.transform.position = new Vector3(5f, 20f, 0);
+        }
+
+        _coordsCurrentFigure = new Transform[_currentFigure.transform.childCount];
+
+        for (int i = 0; i < _coordsCurrentFigure.Length; i++)
+        {
+            _coordsCurrentFigure[i] = _currentFigure.transform.GetChild(i).transform;
+        }
+
+        _nextFigure = ShowNextFigure();
+        _countOfDeleatedLines = 0;
+        _beginTime = Time.time;
+    }
+
+    void Update()
+    {      
+        if(Time.time - _beginTime >= 0.5f)
+        {
+            _isMovingDown = true;
+            _beginTime = Time.time;
+
+            float _x = Input.GetAxisRaw("Horizontal");
+
+            if (_x != 0f)
+            {
+                bool yep = true;
+
+                for (int i = 0; i < _coordsCurrentFigure.Length; i++)
+                {
+                    int x = Mathf.RoundToInt(_coordsCurrentFigure[i].transform.position.x);                    
+                    if (x + _x < 0 || x + _x > 9)
+                    {
+                        yep = false;
+                        break;
+                    }
+                }
+                
+                if(yep) MoveFigureHorizontal(_x);
+            }
+
+            foreach (Transform _t in _coordsCurrentFigure)
+            {
+                int y = Mathf.RoundToInt(_t.transform.position.y);
+                if (y <= 0f)
+                {
+                    SetCurrentFigure(_nextFigure);
+                    _isMovingDown = false;
+                    break;
+                }
+            }
+
+            if(_isMovingDown) MoveFigureDown();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space)) RotateFigure(); 
+    }
+
+    private void MoveFigureDown()
+    {
+        _currentFigure.transform.position += Vector3.down;
+
+        for (int i = 0; i < _coordsCurrentFigure.Length; i++)
+        {            
+            int x = Mathf.RoundToInt(_coordsCurrentFigure[i].transform.position.x);
+            int y = Mathf.RoundToInt(_coordsCurrentFigure[i].transform.position.y);
+            if (_coordXYFigures[x, y] != null)
+            {
+                _currentFigure.transform.position += Vector3.up;
+                SetCurrentFigure(_nextFigure);
+                break;
+            }
+        }
+    }
+
+    private void MoveFigureHorizontal(float _x)
+    {
+        _currentFigure.transform.position += new Vector3(_x, 0f, 0f);
+
+        for (int i = 0; i < _coordsCurrentFigure.Length; i++)
+        {
+            int x = Mathf.RoundToInt(_coordsCurrentFigure[i].transform.position.x);
+            int y = Mathf.RoundToInt(_coordsCurrentFigure[i].transform.position.y);
+            if (_coordXYFigures[x, y] != null)
+            {
+                _currentFigure.transform.position -= new Vector3(_x, 0f, 0f);
+                break;
+            }
+        }
+    }
+
+    private void RotateFigure()
+    {
+        _currentFigure.transform.Rotate(Vector3.forward, 90f);
+
+        for (int i = 0; i < _coordsCurrentFigure.Length; i++)
+        {
+            if(_coordsCurrentFigure[i].transform.position.x > 9 || _coordsCurrentFigure[i].transform.position.x < 0) { _currentFigure.transform.Rotate(Vector3.forward, -90f); break; }
+            
+            int x = Mathf.RoundToInt(_coordsCurrentFigure[i].transform.position.x);
+            int y = Mathf.RoundToInt(_coordsCurrentFigure[i].transform.position.y);
+
+            if(_coordXYFigures[x,y] != null) { _currentFigure.transform.Rotate(Vector3.forward, -90f); break; }
+        }
+    }
+
+    private void SetCurrentFigure(GameObject _figure)
+    {
+        AddCoordinates(); // прежде чем заменить текущую фигуру на новую, надо добавить координаты кубиков в общий список занятых координат
+
+        CheckLines();
+
+        if (_countOfDeleatedLines == 1) { _score += 100;}
+        if (_countOfDeleatedLines == 2) { _score += 300;}
+        if (_countOfDeleatedLines == 3) { _score += 700;}
+        if (_countOfDeleatedLines == 4) { _score += 1500;}
+        
+        _highScoreCount.text = "POINTS: " + _score;
+        _countOfDeleatedLines = 0;
+
+        if (_figure.tag == "NeedCorrectPosition")
+        {
+            _figure.transform.position = new Vector3(4.5f, 20.5f, 0f);
+        }
+        else
+        {
+            _figure.transform.position = new Vector3(5f, 20f, 0);
+        }
+
+        _currentFigure = _figure;
+
+        _coordsCurrentFigure = new Transform[_currentFigure.transform.childCount];
+
+        for (int i = 0; i < _coordsCurrentFigure.Length; i++)
+        {
+            _coordsCurrentFigure[i] = _currentFigure.transform.GetChild(i);
+        }
+
+        _nextFigure = ShowNextFigure();
+    }
+
+    private GameObject ShowNextFigure()
+    {
+        int _randomNextFigure = UnityEngine.Random.Range(0, 7);
+        GameObject _go = Instantiate<GameObject>(_figurePrefabs[_randomNextFigure]);
+        _go.transform.position = new Vector3(25f, 50f, 0f);
+        return _go;
+    }
+
+    private void AddCoordinates()
+    {
+        for (int i = 0; i < _coordsCurrentFigure.Length; i++)
+        {
+            int x = Mathf.RoundToInt(_coordsCurrentFigure[i].transform.position.x);
+            int y = Mathf.RoundToInt(_coordsCurrentFigure[i].transform.position.y);
+            _coordXYFigures[x, y] = _coordsCurrentFigure[i];
+        }
+    }
+
+    private void CheckLines()
+    {
+        //я хер его знает как организовать грамотное удаление 4-х строк, надо подумать
+        for (int y = 0; y < 20; y++)
+        {
+            if (CheckLine(y)) DeleteLine(y);
+        }
+    }
+
+    private void DeleteLine(int y)
+    {
+        for (int x = 0; x < 10; x++)
+        {
+            Destroy(_coordXYFigures[x, y].transform.gameObject);
+        }
+        LineWasDeleted(y);
+        _countOfDeleatedLines++;
+    }
+
+    private bool CheckLine(int y)
+    {
+        bool delete = true;
+
+        for (int x = 9; x >=0 ; x--)
+        {
+            if(_coordXYFigures[x,y] == null)
+            {
+                delete = false;
+                break;
+            }
+        }
+
+        return delete;
+    }
+
+    private void LineWasDeleted(int deleatedLine)
+    {
+        int downLine = deleatedLine + 1;
+
+        for (int y = downLine; y < 20; y++)
+        {
+            for (int x = 0; x < 10; x++)
+            {
+                if(_coordXYFigures[x,y] != null)
+                {
+                    _coordXYFigures[x, y].transform.position += Vector3.down;
+                    _coordXYFigures[x, y - 1] = _coordXYFigures[x, y];
+                    _coordXYFigures[x, y] = null;
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Tetris_Prototype/_Scripts/GameFieldCreator.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameFieldCreator : MonoBehaviour
+{
+    [SerializeField] private GameObject _wallPrefab;
+    [SerializeField] private GameObject _gridPrefab;
+    [SerializeField] private GameObject _wallsAnchor;
+    [SerializeField] private GameObject _gridAnchor;
+
+
+    void Awake()
+    {
+        for (float i = -1f; i <= 10f; i++)
+        {
+            GameObject _cubeWallBottom = Instantiate<GameObject>(_wallPrefab);
+            _cubeWallBottom.transform.position = new Vector3(i, -1f, 0f);
+            _cubeWallBottom.transform.SetParent(_wallsAnchor.transform);;
+        }
+        for (float i = 0f;  i<=19; i++) 
+        {
+            GameObject _cubeWallLeft = Instantiate<GameObject>(_wallPrefab);
+            _cubeWallLeft.transform.position = new Vector3(-1f, i, 0f);
+            _cubeWallLeft.transform.parent = _wallsAnchor.transform;
+
+            GameObject _cubeWallRight = Instantiate<GameObject>(_wallPrefab);
+            _cubeWallRight.transform.position = new Vector3(10f, i, 0f);
+            _cubeWallRight.transform.SetParent(_wallsAnchor.transform);
+        }
+        for (float i = 0f; i <= 19f; i++)
+        {
+            for (float j = 0f; j <= 9f; j++)
+            {
+                GameObject _grid = Instantiate<GameObject>(_gridPrefab);
+                _grid.transform.position = new Vector3(j, i, 0f);
+                _grid.transform.SetParent(_gridAnchor.transform);
+            }
+        }
+    }    
+}
